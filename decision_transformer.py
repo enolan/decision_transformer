494111b5d87c4dfe747ee453a52c4ad239ea395a
Echo Nolan <echo@echonolan.net>
2021-08-11 00:27:50 +0000

    gradient debugging code

--- decision_transformer.py
+++ decision_transformer.py
@@ -5,6 +5,10 @@ import os
 from pathlib import Path
 import PIL
 import pytorch_lightning as pl
+import matplotlib
+from matplotlib.lines import Line2D
+from matplotlib import pyplot as plt
+import numpy as np
 import random
 from taming.models import vqgan
 import torch
@@ -299,6 +303,48 @@ class EvalEveryNIts(pl.Callback):
                 pl_module.logger.experiment.add_image(
                     f"{prompt['prompt']}", img, global_step=pl_module.global_step
                 )
+
+
+class CheckGradients(pl.Callback):
+    def on_after_backward(self, trainer, pl_module):
+        plot_grad_flow(pl_module.named_parameters())
+        plt.savefig(f"grads-{pl_module.global_step}.svg")
+        plt.clf()
+
+
+# Debugging function from https://discuss.pytorch.org/t/check-gradient-flow-in-network/15063/10
+def plot_grad_flow(named_parameters):
+    """Plots the gradients flowing through different layers in the net during training.
+    Can be used for checking for possible gradient vanishing / exploding problems.
+
+    Usage: Plug this function in Trainer class after loss.backwards() as
+    "plot_grad_flow(self.model.named_parameters())" to visualize the gradient flow"""
+    ave_grads = []
+    max_grads = []
+    layers = []
+    for n, p in named_parameters:
+        if (p.requires_grad) and ("bias" not in n) and (p.grad is not None):
+            layers.append(n)
+            ave_grads.append(p.grad.abs().mean().cpu())
+            max_grads.append(p.grad.abs().max().cpu())
+    plt.bar(np.arange(len(max_grads)), max_grads, alpha=0.1, lw=1, color="c")
+    plt.bar(np.arange(len(max_grads)), ave_grads, alpha=0.1, lw=1, color="b")
+    plt.hlines(0, 0, len(ave_grads) + 1, lw=2, color="k")
+    plt.xticks(range(0, len(ave_grads), 1), layers, rotation="vertical")
+    plt.xlim(left=0, right=len(ave_grads))
+    plt.ylim(bottom=-0.001, top=0.02)  # zoom in on the lower gradient regions
+    plt.xlabel("Layers")
+    plt.ylabel("average gradient")
+    plt.title("Gradient flow")
+    plt.grid(True)
+    plt.legend(
+        [
+            Line2D([0], [0], color="c", lw=4),
+            Line2D([0], [0], color="b", lw=4),
+            Line2D([0], [0], color="k", lw=4),
+        ],
+        ["max-gradient", "mean-gradient", "zero-gradient"],
+    )
 
 
 def setup_clip_and_vqgan(want_vqgan_weights=True):
